c73d7e7928101cf879ea32923eee51841d03f181
ShiQing_Chen_Comp <979196931@qq.com>
2018-11-27 18:48:42 +0800

    html

--- src/main/java/com/baida/controller/ApiController.java
+++ src/main/java/com/baida/controller/ApiController.java
@@ -4,13 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
 
 /**
  * @Description //TODO RESTful接口 api
@@ -35,10 +33,21 @@ public class ApiController {
      * @return
      */
     @RequestMapping(value = "/")
-    public ModelAndView helloSpringBootGet(Model model){
+    public String helloSpringBootGet(ModelMap modelMap){
+        modelMap.addAttribute("name","helllo");
         ModelAndView modelAndView = new ModelAndView("index");
-        model.addAttribute("ff", "欢迎进入HTML页面");
-        return modelAndView;
+
+        //判断为pc或mobil
+        return "pc/login";
+    }
+
+    @RequestMapping(value = "/pc/login")
+    public String helloSpringBootGeta(ModelMap modelMap){
+        modelMap.addAttribute("name","helllo");
+        ModelAndView modelAndView = new ModelAndView("pc/login");
+
+        //判断为pc或mobil
+        return "login";
     }
 
 //    /**
@@ -50,14 +59,53 @@ public class ApiController {
 //        return "Hello SpringBootPUT!";
 //    }
 //
-//    /**
-//     * Creat
-//     * @return
-//     */
-//    @RequestMapping(value = "/api",method = RequestMethod.POST)
-//    public String helloSpringBootPost(){
-//        return "Hello SpringBootPOST!";
-//    }
+    /**
+     * Creat
+     * @return
+     */
+    @RequestMapping(value = "/pc/index",method = RequestMethod.POST)
+    public String helloSpringBootPost(){
+
+        return "pc/index";
+    }
+
+    @RequestMapping(value = "/pc/index",method = RequestMethod.GET)
+    public String helloSpringBootH(){
+
+        return "pc/index";
+    }
+    @RequestMapping(value = "/pc/shop",method = RequestMethod.GET)
+    public String helloSpringBoots(){
+
+        return "pc/table_shop";
+    }
+    @RequestMapping(value = "/pc/user",method = RequestMethod.GET)
+    public String helloSpringBootu(){
+
+        return "pc/table_user";
+    }
+    @RequestMapping(value = "/pc/address",method = RequestMethod.GET)
+    public String helloSpringBoota(){
+
+        return "pc/table_address";
+    }
+    @RequestMapping(value = "/pc/goods",method = RequestMethod.GET)
+    public String helloSpringBootg(){
+
+        return "pc/table_goods";
+    }
+    @RequestMapping(value = "/pc/activity",method = RequestMethod.GET)
+    public String helloSpringBooty(){
+
+        return "pc/table_activity";
+    }
+
+    @RequestMapping(value = "/pc/order",method = RequestMethod.GET)
+    public String helloSpringBooto(){
+
+        return "pc/table_order";
+    }
+
 //
 //
 //    /**
